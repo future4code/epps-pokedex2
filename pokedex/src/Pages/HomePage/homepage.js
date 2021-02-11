4c5e6fc47468b3c01aeb2e136b3153539caa103a
Sandro-Tome <sandrotome.jr@gmail.com>
2021-02-11 15:00:17 -0300

    Integração da PokeAPI com a HomePage funcionando.

--- pokedex/src/Pages/HomePage/homepage.js
+++ pokedex/src/Pages/HomePage/homepage.js
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import styled from 'styled-components';
-import PokemonListCards from "../../Components/pokemonListCards";
+import PokemonCard from "../../Components/pokemonCard";
+import UrlsPokedexContext from "../../Contexts/UrlsPokedexContext";
 
 export const MainContainer = styled.div`
-  margin:0;
   padding:0;
+  height: 92vh;
+  margin: 50px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `
 
 const HomePage = () => {
-    return (
-        <div>
-            <PokemonListCards/>
-        </div>
+  const {states, setters, requests} = useContext(UrlsPokedexContext)
+  //const history = useHistory()
+
+  useEffect(() =>{
+    requests.getPokemons()
+  },[requests])
+
+  /* função para adicionar na pokedex */
+  const addPokemonToPokedex = (newItem) => {
+    const index = states.pokedex.findIndex((i) => i.name === newItem.name);
+    let newPokedex = [...states.pokedex];
+    console.log(newPokedex)
+    if(index === -1){
+        newPokedex.push({...states.newPokedex, ammount: 1})
+        alert(`${newItem.name} foi adicionado!`)
+    }else{
+        newPokedex[index].ammount += 1
+    }
+    setters.setPokedex(newPokedex);
+  };
+
+  const pokemonsList = 
+  states.pokemons && states.pokemons.map((item) =>{
+    return(
+      <PokemonCard
+      key={item.url}
+      url={item.url}
+      name={item.name}
+      addPokemon={() => addPokemonToPokedex(item)}
+      />
     )
-}
+  })
+
+  return <MainContainer>{pokemonsList}</MainContainer>;
+};
 
 export default HomePage
--- pokedex/src/Pages/PokemonDetailPage/pokemonDetailPage.js
+++ pokedex/src/Pages/PokemonDetailPage/pokemonDetailPage.js
@@ -1,11 +1,43 @@
 import React from "react";
+import { BASE_URL } from "../../Constants/axios"
+import { useParams } from "react-router-dom"
+import { useRequestData } from "../../Hooks/useRequestData"
 
-const PokeDetailPage = () => {    
+const PokemonDetailPage = () => {
+    const params = useParams()
+    const data = useRequestData(`${BASE_URL}/${params.pokeName}`, {})
+
     return (
         <div>
-            <h1>DETALHES POKEMON</h1>
+            {data.sprites && 
+                <div>
+                    <div>
+                        <img src={data.sprites.front_default} alt=''/> 
+                        <img src={data.sprites.back_default} alt=''/>
+                    </div>
+                    <div>
+                        <div>Poderes</div>
+                        {data.stats.map(detail => {
+                            return <p><strong>{detail.stat.name}: </strong>{detail.base_stat}</p>
+                        })}
+                    </div>
+                    <div>
+                        <div>
+                            {data.types.map(type => {
+                                return <p>{type.type.name}</p>
+                            })}
+                        </div>
+                        <div>
+                            <div>Principais ataques</div>
+                            {data.moves.map((move, index) => {
+                                return index < 5 && <p>{move.move.name}</p>
+                            })}
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
 
-export default PokeDetailPage
+export default PokemonDetailPage
